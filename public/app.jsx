17ca206afcaf90f9c07ea9deb9dbd2b3482d4fbc
joshgressman <joshgressman@gmail.com>
2017-02-06 20:20:52 -0600

    added greeter component

--- public/app.jsx
+++ public/app.jsx
@@ -1,7 +1,22 @@
+var Greeter = React.createClass({
+render: function () {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <p>This is from the component</p>
+   </div>
 
+  );
+}
 
+}); //Component Creation
 
+//Will render the Greeter div with JSX to the DOM
   ReactDOM.render(
-    <h1>Hello World!</h1>,
+     <Greeter/>,
     document.getElementById('app')
   );
+
+
+
+//This is a React component create in Greeter create clas
