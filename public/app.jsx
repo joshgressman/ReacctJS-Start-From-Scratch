d7c5e34827471a333e33b19c60c9c21c485cfc93
joshgressman <joshgressman@gmail.com>
2017-02-23 06:08:24 -0600

    stuff

--- public/app.jsx
+++ public/app.jsx
@@ -1,10 +1,11 @@
 var GreeterMessage = React.createClass({ //this is a nested component
 render: function (){
   var name = this.props.name;
+  var message = this.props.message;
   return (
     <div>
       <h1>Hello {name}!</h1>
-      <p>Some Paragraph</p>
+      <p>{message}</p>
     </div>
   );
 }
@@ -36,12 +37,13 @@ var Greeter = React.createClass({
   getDefaultProps: function () { //default info
       return {
         name: 'React',
-        message: 'This is the default message'
+        message: 'This is a default message'
       };
   },
 getInitialState: function () {
   return {
-    name: this.props.name
+    name: this.props.name,
+    message: this.props.message
   };
 },
 handleNewName: function (name) {
@@ -55,7 +57,7 @@ render: function () {
   var message = this.props.message;
   return (
     <div>
-    <GreeterMessage name={name}/>
+    <GreeterMessage name={name} message={message}/>
     <GreeterForm onNewName={this.handleNewName}/>
    </div>
 
